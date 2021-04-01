c910b05fc6b52dc7daf18ccf19df089fddc20411
Arlo Gilbert <a@osano.com>
2021-04-01 12:22:00 -0500

    final version

--- Shopping List/ViewController.swift
+++ Shopping List/ViewController.swift
@@ -8,26 +8,36 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController, WKNavigationDelegate {
-    var webView: WKWebView!
+class ViewController: UIViewController, WKUIDelegate {
+
+    @IBOutlet var webView: WKWebView!
+    @IBOutlet weak var webViewContainer: UIView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // 1
+        //overrideUserInterfaceStyle = .dark
+
+        /* set up the webview */
+        webView.uiDelegate = self // Move delegate here
+        webViewContainer.translatesAutoresizingMaskIntoConstraints = true
+
+        // view behaviors & styles
+        //webView.isOpaque = true
+        //webView.backgroundColor = UIColor.clear
+        //webView.backgroundColor = UIColor(red:0.11, green:0.13, blue:0.19, alpha:1)
+        webView.backgroundColor = UIColor.systemBackground
+        webView.scrollView.backgroundColor = UIColor.systemBackground
+        webView.scrollView.bounces = true
+        webViewContainer.insetsLayoutMarginsFromSafeArea = true
+
+        // view heights & widths
+        webViewContainer.clipsToBounds = true
+        webView.clipsToBounds = true
+        webViewContainer.frame = CGRect.init(x: 0, y: UIApplication.shared.statusBarFrame.height, width: view.bounds.maxX, height: view.bounds.maxY)
+        webView.frame = CGRect.init(x: 0, y: 0, width: webViewContainer.bounds.maxX, height: webViewContainer.bounds.maxY)
+        /* load Google Shopping */
         let url = URL(string: "https://shoppinglist.google.com/")!
-        webView.load(URLRequest(url: url))
-          
-        // 2
-        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
-        toolbarItems = [refresh]
-        navigationController?.isToolbarHidden = true
+        let request = URLRequest(url: url)
+        webView.load(request)
     }
-    override func loadView() {
-        webView = WKWebView()
-        webView.navigationDelegate = self
-        view = webView
-    }
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        title = webView.title
-        }
 }
